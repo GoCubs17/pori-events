dbc1342877d787195386ca9a570b9791ae325e58
Tuukka Turu <tuukka.turu@druid.fi>
2018-02-06 12:47:13 +0200

    PORI-229: Added slideshow things

--- drupal/web/themes/custom/pori_events/src/js/scripts.js
+++ drupal/web/themes/custom/pori_events/src/js/scripts.js
@@ -10,4 +10,25 @@
   //   }
   // };
 
+  Drupal.behaviors.slickSlider = {
+    attach: function (context) {
+      $('.slide-container__content .field__items', context).once('slickSlider').slick({
+        autoplay: true,
+        autoplaySpeed: 6000,
+        pauseOnHover: true,
+        infinite: true,
+        slidesToShow: 1,
+        customPaging: function (slick, index) {
+          return '<a>' + index + '</a>';
+        },
+        dots: true,
+        dotsClass: 'slick-dots',
+        prevArrow: '<a data-role="none" class="slide__arrow-prev" ></a>',
+        nextArrow: '<a data-role="none" class="slide__arrow-next" ></a>',
+        appendArrows: $('.slide-container__arrows'),
+        appendDots: $('.slide-container__dots')
+      });
+    }
+  }
+
 })(jQuery, Drupal);
