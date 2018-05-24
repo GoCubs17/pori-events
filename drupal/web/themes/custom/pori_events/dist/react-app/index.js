14ba23790f5acd05c335252149af54eaf33ccc39
Matti Hernesniemi <matti.hernesniemi@wunder.io>
2018-05-24 14:36:18 +0300

    Implement new version of searchkit react app for development

---
+++ drupal/web/themes/custom/pori_events/dist/react-app/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+let drupalElem = 'kada-event-search';
+let devElem = 'root';
+
+
+ReactDOM.render(
+  <App />,
+  document.getElementById(drupalElem)
+);
