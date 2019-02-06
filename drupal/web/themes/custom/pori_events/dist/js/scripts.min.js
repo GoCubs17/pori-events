984b78b9acc5a92a9b364c8523baa10e9bf90302
Arturs Vilkajs <arturs.vilkajs@wunder.io>
2019-02-06 15:38:01 +0200

    Remove Slick library bits from the theme.

--- drupal/web/themes/custom/pori_events/dist/js/scripts.min.js
+++ drupal/web/themes/custom/pori_events/dist/js/scripts.min.js
@@ -2,6 +2,12 @@
 
   'use strict';
 
-
+  // Drupal.behaviors.exampleBehavior = {
+  //   attach: function (context) {
+  //     $('.example', context).once('exampleBehavior').each(function () {
+  //       // Do things.
+  //     });
+  //   }
+  // };
 
 })(jQuery, Drupal);
