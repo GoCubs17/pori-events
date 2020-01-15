54e3cbf1ae526eec1b8e47a6a05ea1a1c211b03a
Tormi Tabor <tormi.tabor@gmail.com>
2020-01-15 16:35:54 +0200

    PORISD-43: Replace --missing-from-source  with --sync.

--- drupal/.lando/migrate.sh
+++ drupal/.lando/migrate.sh
@@ -4,5 +4,4 @@ set -exu
 # This file manages event node import and cleanup.
 
 # Run migrate commands.
-drush @tapahtumat.local migrate:import --group=migrate_source_event --update
-drush @tapahtumat.local migrate:rollback --group=migrate_source_event --missing-from-source
+drush @tapahtumat.local migrate:import --group=migrate_source_event --update --sync
